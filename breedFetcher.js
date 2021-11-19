031b11bb8d756fad5dd10697c51cbfe94bcc0058
Bryson Best <bryson.best@gmail.com>
2021-11-18 17:27:22 -0800

    Refactored function to use a callback and return breedname through index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,22 @@
 const request = require('request');
-let cat = process.argv.slice(2);
-cat = cat.join("_");
-const url = "https://api.thecatapi.com/v1/breeds/search?q=";
-const search = url + cat;
 
-request(search, (error, response, body) => {
-  if (error) {
-    return console.log(error);
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    return console.log('No Cat Found.');
-  }
-  console.log(data[0]['description']);
-});
+//const url = "https://api.thecatapi.com/v1/breeds/search?q=";
+//const search = url + cat;
 
+const fetchBreedDescription = function(breedName, callback) {
+  let search = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  
+  request(search, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback(error, 'No Cat Found.');
+    } else {
+      callback(null, data[0]['description']);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
